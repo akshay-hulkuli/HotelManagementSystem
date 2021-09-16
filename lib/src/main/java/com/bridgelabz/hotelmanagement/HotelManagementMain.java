6bbc1de65a7bd472c2e62e2932481c0692459f60
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-16 08:40:41 +0530

    REFACTORED: the condition to return boolean value

--- lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
+++ lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
@@ -6,10 +6,14 @@ public class HotelManagementMain {
 	List<Hotel> hotelList = new LinkedList<Hotel>();
 
 	public boolean  addHotel(String name, double weekdayPrice, double weekendPrice) {
-		Hotel hotel = new Hotel(name, weekdayPrice, weekendPrice);
-		System.out.println(hotel);
-		hotelList.add(hotel);
-		return true;
+		
+			Hotel hotel = new Hotel(name, weekdayPrice, weekendPrice);
+			System.out.println(hotel);
+			int oldSize = hotelList.size();
+			hotelList.add(hotel);
+			if(oldSize<hotelList.size())
+			return true;
+			return false;
 	}
 
 }
