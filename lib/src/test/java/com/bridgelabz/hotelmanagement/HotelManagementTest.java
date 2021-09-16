995d96da9f4da2512e2af345712d1bc7d0055c03
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-16 11:43:46 +0530

    REFACTORED: junit method to test addHotel method

--- lib/src/test/java/com/bridgelabz/hotelmanagement/HotelManagementTest.java
+++ lib/src/test/java/com/bridgelabz/hotelmanagement/HotelManagementTest.java
@@ -2,26 +2,28 @@ package com.bridgelabz.hotelmanagement;
 
 import org.junit.Assert;
 import org.junit.Test;
-import java.time.LocalDate;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class HotelManagementTest {
 	@Test
 	public void givenHotelData_ifCorrect_ShouldGetTrue() {
 		HotelManagementMain hotelManagement = new HotelManagementMain();
 		int oldSize = hotelManagement.hotelList.size();
-		hotelManagement.addHotel("Lakewood",100.0,4);
+		hotelManagement.addHotel("Lakewood",100.0,110.0,4);
 		Assert.assertSame(oldSize+1,hotelManagement.hotelList.size());
 	}
 	
 	@Test
 	public void givenDate_find_CheapestHotel() {
 		HotelManagementMain hotelManagement = new HotelManagementMain();
-		hotelManagement.addHotel("Lakewood",100.0,4);
-		hotelManagement.addHotel("Bridgewood",110.0,4);
-		hotelManagement.addHotel("Ridgewood",105.0,4);
-		LocalDate date1 = LocalDate.of(2021, 9, 10);
-		LocalDate date2 = LocalDate.of(2021, 9, 11);
-		Hotel hotel = hotelManagement.getCheapestHotel(date1, date2);
+		hotelManagement.addHotel("Lakewood",110.0,90.0,3);
+		hotelManagement.addHotel("Bridgewood",150.0,50.0,4);
+		hotelManagement.addHotel("Ridgewood",220.0,150.0,5);
+		
+		Hotel hotel = hotelManagement.getCheapestHotel("10-9-2021","20-10-2021");
+		System.out.println("the cheapest hotel is : ");
 		System.out.println(hotel);
 	}
 }
