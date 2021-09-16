3d3f6e9726d4d439d787afc3032013b5a6475351
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-16 11:42:28 +0530

    REFACTORED: addHotel method to take two prices

--- lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
+++ lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
@@ -1,21 +1,30 @@
 package com.bridgelabz.hotelmanagement;
 
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 public class HotelManagementMain {
 	
 	List<Hotel> hotelList = new ArrayList<Hotel>();
 	
-	public void  addHotel(String name, double regularprice, int rating) {
-		Hotel hotel = new Hotel(name, regularprice, rating);
+	public void  addHotel(String name, double weekdayPrice, double weekendPrice, int rating) {
+		Hotel hotel = new Hotel(name, weekdayPrice, weekendPrice, rating);
 		hotelList.add(hotel);
 		System.out.println(hotel);
 	}
 	
-	public Hotel getCheapestHotel(LocalDate date1, LocalDate date2) {
-		Period period = Period.between(date1, date2);
-		int numOfDays = period.getDays();
+	public Hotel getCheapestHotel(String date1, String date2) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy");
+		Date startDate = null, endDate = null;
+		try {
+			startDate = sdf.parse(date1);
+			endDate = sdf.parse(date2);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		long diff = endDate.getTime() - startDate.getTime();
+		long numOfDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 		return hotelList.stream().min((h1,h2) -> h1.getPrice(numOfDays).compareTo(h2.getPrice(numOfDays))).orElse(null);
 	}
 
