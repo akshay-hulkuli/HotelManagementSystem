c5ab8dd8035c1d6e347ec714100db97b93c466f3
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-16 19:18:23 +0530

    REFACTORED: junit method to add reward prices to class

--- lib/src/test/java/com/bridgelabz/hotelmanagement/HotelManagementTest.java
+++ lib/src/test/java/com/bridgelabz/hotelmanagement/HotelManagementTest.java
@@ -10,46 +10,51 @@ public class HotelManagementTest {
 	public void givenHotelData_ifCorrect_ShouldGetTrue() {
 		HotelManagementMain hotelManagement = new HotelManagementMain();
 		int oldSize = hotelManagement.hotelList.size();
-		hotelManagement.addHotel("Lakewood",100.0,110.0,4);
+		hotelManagement.addHotel("Lakewood",100.0,110.0,3,80.0,80.0);
 		Assert.assertSame(oldSize+1,hotelManagement.hotelList.size());
 	}
 	
 	@Test
 	public void givenDateRange_find_CheapestHotel() {
 		HotelManagementMain hotelManagement = new HotelManagementMain();
-		hotelManagement.addHotel("Lakewood",110.0,90.0,3);
-		hotelManagement.addHotel("Bridgewood",150.0,50.0,4);
-		hotelManagement.addHotel("Ridgewood",220.0,150.0,5);
+		hotelManagement.addHotel("Lakewood",110,90,3,80,80);
+		hotelManagement.addHotel("Bridgewood",150,50,4,110,50);
+		hotelManagement.addHotel("Ridgewood",220,150,5,100,40);
 		
-		List<Hotel> hotels = hotelManagement.getCheapestHotel("11-9-2020","12-9-2020");
+		List<Hotel> hotels = hotelManagement.getCheapestHotel("11Sep2020","12Sep2020");
+		
 		System.out.println("the cheapest hotel is/are : ");
 		System.out.println(hotels);
 		System.out.println();
+		Assert.assertEquals("Lakewood", hotels.get(0).getName());
+		Assert.assertEquals("Bridgewood", hotels.get(1).getName());
 	}
 	
 	@Test
 	public void givenDateRange_find_CheapestAndBestRatedHotel() {
 		HotelManagementMain hotelManagement = new HotelManagementMain();
-		hotelManagement.addHotel("Lakewood",110.0,90.0,3);
-		hotelManagement.addHotel("Bridgewood",150.0,50.0,4);
-		hotelManagement.addHotel("Ridgewood",220.0,150.0,5);
+		hotelManagement.addHotel("Lakewood",110,90,3,80,80);
+		hotelManagement.addHotel("Bridgewood",150,50,4,110,50);
+		hotelManagement.addHotel("Ridgewood",220,150,5,100,40);
 		
-		Hotel hotel = hotelManagement.getCheapestAndBestRatedHotel("11-9-2020","12-9-2020");
+		Hotel hotel = hotelManagement.getCheapestAndBestRatedHotel("11Sep2020","12Sep2020");
 		System.out.println("the cheapest and best rated hotel is : ");
 		System.out.println(hotel);
 		System.out.println();
+		Assert.assertEquals("Bridgewood", hotel.getName());
 	}
 	
 	@Test
 	public void givenDateRange_find_BestRatedHotel() {
 		HotelManagementMain hotelManagement = new HotelManagementMain();
-		hotelManagement.addHotel("Lakewood",110.0,90.0,3);
-		hotelManagement.addHotel("Bridgewood",150.0,50.0,4);
-		hotelManagement.addHotel("Ridgewood",220.0,150.0,5);
+		hotelManagement.addHotel("Lakewood",110,90,3,80,80);
+		hotelManagement.addHotel("Bridgewood",150,50,4,110,50);
+		hotelManagement.addHotel("Ridgewood",220,150,5,100,40);
 		
-		Hotel hotel = hotelManagement.getBestRatedHotel("11-9-2020","12-9-2020");
+		Hotel hotel = hotelManagement.getBestRatedHotel("11Sep2020","12Sep2020");
 		System.out.println("the best rated hotel is : ");
 		System.out.println(hotel);
 		System.out.println();
+		Assert.assertEquals("Ridgewood", hotel.getName());
 	}
 }
