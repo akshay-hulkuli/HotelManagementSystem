46440e8a66585519e25db3113fb952714f3e893a
Akshaya HP <60184210+akshay-hulkuli@users.noreply.github.com>
2021-09-16 21:25:40 +0530

    REFACTORED: changed test case name

--- lib/src/test/java/com/bridgelabz/hotelmanagement/HotelManagementTest.java
+++ lib/src/test/java/com/bridgelabz/hotelmanagement/HotelManagementTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class HotelManagementTest {
 	@Test
-	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+	public void givenHotelData_WhenProper_ShouldReturnTrue() {
 		HotelManagementMain hotelmanagement = new HotelManagementMain();
 		boolean result = hotelmanagement.addHotel("Lakewood",100.0,110.0);
 		Assert.assertTrue(result);
