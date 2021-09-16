afc0d407b98d36acf0974946e6f878bd043f5e73
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-16 09:52:29 +0530

    ADDED: method to find cheapest hotel

--- lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
+++ lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
@@ -1,19 +1,22 @@
 package com.bridgelabz.hotelmanagement;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.*;
 public class HotelManagementMain {
 	
-	List<Hotel> hotelList = new LinkedList<Hotel>();
-
-	public boolean  addHotel(String name, double weekdayPrice, double weekendPrice) {
-		
-			Hotel hotel = new Hotel(name, weekdayPrice, weekendPrice);
-			System.out.println(hotel);
-			int oldSize = hotelList.size();
-			hotelList.add(hotel);
-			if(oldSize<hotelList.size())
-			return true;
-			return false;
+	List<Hotel> hotelList = new ArrayList<Hotel>();
+	
+	public void  addHotel(String name, double regularprice, int rating) {
+		Hotel hotel = new Hotel(name, regularprice, rating);
+		hotelList.add(hotel);
+		System.out.println(hotel);
+	}
+	
+	public Hotel getCheapestHotel(LocalDate date1, LocalDate date2) {
+		Period period = Period.between(date1, date2);
+		int numOfDays = period.getDays();
+		return hotelList.stream().min((h1,h2) -> h1.getPrice(numOfDays).compareTo(h2.getPrice(numOfDays))).orElse(null);
 	}
 
 }
