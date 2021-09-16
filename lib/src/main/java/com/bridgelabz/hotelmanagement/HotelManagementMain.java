5fe593f6230481f305c88c8e8308227a749efac4
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-16 12:45:33 +0530

    REFACTORED: to find cheapest hotel based on weekdays and weekends

--- lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
+++ lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementMain.java
@@ -2,11 +2,15 @@ package com.bridgelabz.hotelmanagement;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.function.*;
 public class HotelManagementMain {
 	
 	List<Hotel> hotelList = new ArrayList<Hotel>();
+	int numOfWeekdays =0, numOfWeekends =0;
+	
 	
 	public void  addHotel(String name, double weekdayPrice, double weekendPrice, int rating) {
 		Hotel hotel = new Hotel(name, weekdayPrice, weekendPrice, rating);
@@ -14,18 +18,35 @@ public class HotelManagementMain {
 		System.out.println(hotel);
 	}
 	
-	public Hotel getCheapestHotel(String date1, String date2) {
+	public List<Hotel> getCheapestHotel(String date1, String date2) {
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy");
-		Date startDate = null, endDate = null;
+		Calendar startDate = Calendar.getInstance(), endDate = Calendar.getInstance();
 		try {
-			startDate = sdf.parse(date1);
-			endDate = sdf.parse(date2);
+			startDate.setTime(sdf.parse(date1));
+			endDate.setTime(sdf.parse(date2));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		long diff = endDate.getTime() - startDate.getTime();
-		long numOfDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-		return hotelList.stream().min((h1,h2) -> h1.getPrice(numOfDays).compareTo(h2.getPrice(numOfDays))).orElse(null);
+	
+		do {
+			if(startDate.get(Calendar.DAY_OF_WEEK) == 0 || startDate.get(Calendar.DAY_OF_WEEK) == 6) numOfWeekends++;
+			else numOfWeekdays++;
+			startDate.add(Calendar.DATE,1);
+		} while(startDate.compareTo(endDate) <=0);
+		
+		Hotel cheapestHotel = hotelList.stream()
+							.min((h1,h2) -> h1.getPrice(numOfWeekdays,numOfWeekends).compareTo(h2.getPrice(numOfWeekdays,numOfWeekends)))
+							.orElse(null);
+		
+		double cheapestPrice = cheapestHotel.getPrice(numOfWeekdays,numOfWeekends);
+		System.out.println("the minimum price is : "+cheapestPrice);
+		Predicate<Hotel> isMinimum = (hotel) -> (hotel.getPrice(numOfWeekdays,numOfWeekends) == cheapestPrice)?true:false; 
+		List<Hotel> cheapestHotels = hotelList.stream()
+									 .filter(isMinimum)
+									 .collect(Collectors.toList());
+		return cheapestHotels;
+
 	}
 
 }
