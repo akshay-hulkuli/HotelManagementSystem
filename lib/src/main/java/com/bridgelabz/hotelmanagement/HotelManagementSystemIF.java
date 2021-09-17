64e5f98ddeb98b76d3d9d6ac7c86b151f699a79d
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-17 19:42:00 +0530

    ADDED: interface that defines services provided by system

---
+++ lib/src/main/java/com/bridgelabz/hotelmanagement/HotelManagementSystemIF.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.hotelmanagement;
+
+public interface HotelManagementSystemIF {
+	public void  addHotel(Hotel hotel);
+	public Hotel getCheapestHotel(String date1, String date2);
+}
